02a7a1979f39a84efa77a6109374055b04d36ff8
Ubuntu <bianka@Bianka.53h43fzh542u3d5fbmcsftj20f.bx.internal.cloudapp.net>
2023-10-17 08:08:17 +0000

    [FEATURE] Splited blockchain.py with block.py

--- blockchain.py
+++ blockchain.py
@@ -1,14 +1,4 @@
-class Block:
-    """
-    Block: a unit of storage.
-    Store transactions in a blockchain that supports a cryptocurrency.
-    """
-
-    def __init__(self, data):
-        self.data = data
-
-    def __repr__(self):
-        return f'Block - data: {self.data}'
+from block import Block
 
 class Blockchain:
     """
